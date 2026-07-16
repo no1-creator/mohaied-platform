9de3f2a646e8f42f5b85dcdfecb8e5e1a2081125
no1-creator <yasin.thabet88@gmail.com>
2026-07-16 19:02:14 +0300

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     PrismaModule,
     AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
