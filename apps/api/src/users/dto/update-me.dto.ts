a2acf535b37554850bdf5c9dfada8399c33f4f09
no1-creator <yasin.thabet88@gmail.com>
2026-07-18 17:54:28 +0300

    Create update-me.dto.ts

---
+++ apps/api/src/users/dto/update-me.dto.ts
@@ -0,0 +1,17 @@
+import { IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class UpdateMeDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(120)
+  fullName?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(30)
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  avatarUrl?: string;
+}
